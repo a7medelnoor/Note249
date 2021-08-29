375057c944e2dfddf3989a45160d8bb8952678ae
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-08-29 19:59:20 +0200

    start activity for result got to create note activity

--- app/src/main/java/com/a7medelnoor/note249/MainActivity.java
+++ app/src/main/java/com/a7medelnoor/note249/MainActivity.java
@@ -1,14 +1,27 @@
 package com.a7medelnoor.note249;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_CODE_ADD_NOTE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageView = findViewById(R.id.imageAddImage);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(new
+                                Intent(getApplicationContext(), CreateNoteActivity.class),
+                        REQUEST_CODE_ADD_NOTE);
+            }
+        });
     }
 }
