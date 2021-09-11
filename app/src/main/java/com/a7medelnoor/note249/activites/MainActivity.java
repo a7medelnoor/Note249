0421f0363741532a80721952adf6a051625ade31
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-09-12 01:21:14 +0200

    handle the add and update and show notes

--- app/src/main/java/com/a7medelnoor/note249/activites/MainActivity.java
+++ app/src/main/java/com/a7medelnoor/note249/activites/MainActivity.java
@@ -1,23 +1,40 @@
 package com.a7medelnoor.note249.activites;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import com.a7medelnoor.note249.R;
-import com.a7medelnoor.note249.activites.CreateNoteActivity;
+import com.a7medelnoor.note249.adapters.NoteAdapter;
+import com.a7medelnoor.note249.database.NoteDataBase;
+import com.a7medelnoor.note249.entities.Note;
+import com.a7medelnoor.note249.listeners.Listeners;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements Listeners {
     public static final int REQUEST_CODE_ADD_NOTE = 1;
+    public static final int REQUEST_CODE_UPDATE_NOTE = 2;
+    public static final int REQUEST_CODE_SHOW_NOTES = 3;
+    private RecyclerView noteRecyclerView;
+    private List<Note> noteList;
+    private NoteAdapter noteAdapter;
+    private int noteClickedPosition = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView imageView = findViewById(R.id.imageAddImage);
+        ImageView imageView = findViewById(R.id.imageAddNoteButton);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,5 +43,62 @@ public class MainActivity extends AppCompatActivity {
                         REQUEST_CODE_ADD_NOTE);
             }
         });
+        noteRecyclerView = findViewById(R.id.noteRecyclerView);
+        noteRecyclerView.setLayoutManager(
+                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        );
+        noteList = new ArrayList<>();
+        noteAdapter = new NoteAdapter(noteList, this);
+        noteRecyclerView.setAdapter(noteAdapter);
+        getNotes(REQUEST_CODE_SHOW_NOTES);
+    }
+
+    private void getNotes(final int requestCode) {
+        @SuppressLint("StaticFieldLeak")
+        class getNotesTask extends AsyncTask<Void, Void, List<Note>> {
+            @Override
+            protected List<Note> doInBackground(Void... voids) {
+                return NoteDataBase.getNoteDataBase(getApplicationContext()).noteDao().getAllNotes();
+            }
+
+            @Override
+            protected void onPostExecute(List<Note> notes) {
+                super.onPostExecute(notes);
+                if (requestCode == REQUEST_CODE_SHOW_NOTES){
+                    noteList.addAll(notes);
+                    noteAdapter.notifyDataSetChanged();
+                }else if (requestCode == REQUEST_CODE_ADD_NOTE){
+                    noteList.add(0,notes.get(0));
+                    noteAdapter.notifyItemInserted(0);
+                    noteRecyclerView.smoothScrollToPosition(0);
+                }else if (requestCode == REQUEST_CODE_UPDATE_NOTE){
+                    noteList.remove(noteClickedPosition);
+                    noteList.add(noteClickedPosition, notes.get(noteClickedPosition));
+                    noteAdapter.notifyItemChanged(noteClickedPosition);
+                }
+            }
+        }
+        new getNotesTask().execute();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_ADD_NOTE && resultCode == RESULT_OK) {
+            getNotes(REQUEST_CODE_ADD_NOTE);
+        }else if (requestCode == REQUEST_CODE_UPDATE_NOTE && resultCode == RESULT_OK){
+            if (data != null){
+                getNotes(REQUEST_CODE_UPDATE_NOTE);
+            }
+        }
+    }
+
+    @Override
+    public void onNoteClicked(Note note, int position) {
+    noteClickedPosition = position;
+    Intent intent = new Intent(getApplicationContext(), CreateNoteActivity.class);
+    intent.putExtra("isViewOrUpdate", true);
+    intent.putExtra( "note",note);
+    startActivityForResult(intent,REQUEST_CODE_UPDATE_NOTE);
     }
 }
