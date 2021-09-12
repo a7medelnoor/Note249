ba7a8b2aa17a6d23e93a527f3a4daba067d64d8c
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-09-12 15:42:41 +0200

    configure note editing and deletion

--- app/src/main/java/com/a7medelnoor/note249/activites/MainActivity.java
+++ app/src/main/java/com/a7medelnoor/note249/activites/MainActivity.java
@@ -50,10 +50,10 @@ public class MainActivity extends AppCompatActivity implements Listeners {
         noteList = new ArrayList<>();
         noteAdapter = new NoteAdapter(noteList, this);
         noteRecyclerView.setAdapter(noteAdapter);
-        getNotes(REQUEST_CODE_SHOW_NOTES);
+        getNotes(REQUEST_CODE_SHOW_NOTES, false);
     }
 
-    private void getNotes(final int requestCode) {
+    private void getNotes(final int requestCode, final boolean isNoteDeleted) {
         @SuppressLint("StaticFieldLeak")
         class getNotesTask extends AsyncTask<Void, Void, List<Note>> {
             @Override
@@ -64,17 +64,22 @@ public class MainActivity extends AppCompatActivity implements Listeners {
             @Override
             protected void onPostExecute(List<Note> notes) {
                 super.onPostExecute(notes);
-                if (requestCode == REQUEST_CODE_SHOW_NOTES){
+                if (requestCode == REQUEST_CODE_SHOW_NOTES) {
                     noteList.addAll(notes);
                     noteAdapter.notifyDataSetChanged();
-                }else if (requestCode == REQUEST_CODE_ADD_NOTE){
-                    noteList.add(0,notes.get(0));
+                } else if (requestCode == REQUEST_CODE_ADD_NOTE) {
+                    noteList.add(0, notes.get(0));
                     noteAdapter.notifyItemInserted(0);
                     noteRecyclerView.smoothScrollToPosition(0);
-                }else if (requestCode == REQUEST_CODE_UPDATE_NOTE){
+                } else if (requestCode == REQUEST_CODE_UPDATE_NOTE) {
                     noteList.remove(noteClickedPosition);
-                    noteList.add(noteClickedPosition, notes.get(noteClickedPosition));
-                    noteAdapter.notifyItemChanged(noteClickedPosition);
+
+                    if (isNoteDeleted) {
+                        noteAdapter.notifyItemRemoved(noteClickedPosition);
+                    }else {
+                        noteList.add(noteClickedPosition, notes.get(noteClickedPosition));
+                        noteAdapter.notifyItemChanged(noteClickedPosition);
+                    }
                 }
             }
         }
@@ -85,20 +90,20 @@ public class MainActivity extends AppCompatActivity implements Listeners {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_CODE_ADD_NOTE && resultCode == RESULT_OK) {
-            getNotes(REQUEST_CODE_ADD_NOTE);
-        }else if (requestCode == REQUEST_CODE_UPDATE_NOTE && resultCode == RESULT_OK){
-            if (data != null){
-                getNotes(REQUEST_CODE_UPDATE_NOTE);
+            getNotes(REQUEST_CODE_ADD_NOTE, false);
+        } else if (requestCode == REQUEST_CODE_UPDATE_NOTE && resultCode == RESULT_OK) {
+            if (data != null) {
+                getNotes(REQUEST_CODE_UPDATE_NOTE, data.getBooleanExtra("isNoteDeleted", false));
             }
         }
     }
 
     @Override
     public void onNoteClicked(Note note, int position) {
-    noteClickedPosition = position;
-    Intent intent = new Intent(getApplicationContext(), CreateNoteActivity.class);
-    intent.putExtra("isViewOrUpdate", true);
-    intent.putExtra( "note",note);
-    startActivityForResult(intent,REQUEST_CODE_UPDATE_NOTE);
+        noteClickedPosition = position;
+        Intent intent = new Intent(getApplicationContext(), CreateNoteActivity.class);
+        intent.putExtra("isViewOrUpdate", true);
+        intent.putExtra("note", note);
+        startActivityForResult(intent, REQUEST_CODE_UPDATE_NOTE);
     }
 }
